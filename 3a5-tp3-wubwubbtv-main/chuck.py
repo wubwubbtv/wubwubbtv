bdca903af5f65b55821ea7dde2f284c7866d05d3
Louis <1325866@cegepmontpetit.ca>
2021-12-21 21:24:48 -0500

    Question1-7

--- 3a5-tp3-wubwubbtv-main/chuck.py
+++ 3a5-tp3-wubwubbtv-main/chuck.py
@@ -11,30 +11,55 @@
 
 import requests, json, sys, argparse
 
+from requests.models import HTTPError
+
 parser = argparse.ArgumentParser()
 parser.add_argument("jokeID",type=str, nargs="?")
-parser.add_argument("--category","-c",type=str, help= "categories possible")
-parser.add_argument("--list-category","-l",action="store_true",help="Liste des category")
+parser.add_argument("--category","-c",type=str, help= "catégories disponibles")
+parser.add_argument("--list-category","-l",action="store_true",help="Liste des catégories disponibles")
+parser.add_argument("--search","-s",type=str, help= "on cherche des blagues contenant le mot passé en argument")
 args = parser.parse_args()
-#tableauCategorie = requests.get("https://api.chucknorris.io/jokes/categories")
-#print(tableauCategorie.json())
-print(args)
+tableauCategorie = requests.get("https://api.chucknorris.io/jokes/categories")
+listtableuacategorie = tableauCategorie.json()
+
 def chuck():
+
     if args.category is not None:
         data = requests.get(f"https://api.chucknorris.io/jokes/random?category={args.category}")
-    #if args.list-category is not None:
+        tt = json.loads(data.text)
+        print(tt["value"],str("["+tt["id"]+"]"))
+    
+    elif args.list_category:
+        print("Catégories disponibles:")
+        for i in range(len(listtableuacategorie)):
+            print("- " + listtableuacategorie[i])
+    
+    elif args.search is not None:
+        data = requests.get(f"https://api.chucknorris.io/jokes/search?query={args.search}")
+        tt =  json.loads(data.text)
+        print(str(tt["total"]) + " résultats: ")
+        for i in range(len(tt)):
+            print("- ",tt["result"][i]["value"],str("["+tt["result"][i]["id"]+"]"))
+
+    
+
     else:
         data = requests.get("https://api.chucknorris.io/jokes/random")
-    tt = json.loads(data.text)
-    print(tt["value"])
+        tt = json.loads(data.text)
+        print(tt["value"],str("["+tt["id"]+"]"))
+    
 
 def chuckAvecJoke():
     
     data = requests.get(f"https://api.chucknorris.io/jokes/{args.jokeID}")
     tt = json.loads(data.text)
     print(tt["value"])
-
-if args.jokeID is not None:
-    chuckAvecJoke()
-else:
-    chuck()
+try:
+    if args.jokeID is not None:
+        chuckAvecJoke()
+    else:
+        chuck()
+except HTTPError as e:
+    print("e")
+except:
+    print("error 404")
